1c988fa609ca7f5b2c669ffc76533ae2771888f1
FluffleFox <44639764+FluffleFox@users.noreply.github.com>
2019-12-25 20:49:41 +0100

    PathFind

---
+++ Assets/Scripts/AbominacjaRobiacaZaPathFind.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbominacjaRobiacaZaPathFind : MonoBehaviour
+{
+    public Transform Target;
+    public float Delay;
+    float delay;
+    private void Start()
+    {
+        delay = Delay;
+    }
+    void Update()
+    {
+        if(Vector3.Distance(Target.position, transform.position) > 1.2f)
+        {
+            if (delay <= 0)
+            {
+                Vector3 Way = Target.position - transform.position;
+                if (Mathf.Abs(Way.x) > Mathf.Abs(Way.z)) { transform.position += Vector3.right * Mathf.Sign(Way.x); }
+                else { transform.position += Vector3.forward * Mathf.Sign(Way.z); }
+                delay = Delay;
+            }
+            delay -= Time.deltaTime;
+        }
+    }
+}
