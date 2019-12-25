1171488330df6b1f200b4078e987f4749e7853f0
FluffleFox <44639764+FluffleFox@users.noreply.github.com>
2019-12-25 21:13:25 +0100

    PoprawionyPathfinding

--- Assets/Scripts/AbominacjaRobiacaZaPathFind.cs
+++ Assets/Scripts/AbominacjaRobiacaZaPathFind.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class AbominacjaRobiacaZaPathFind : MonoBehaviour
 {
-    public Transform Target;
+    Transform Target;
     public float Delay;
     float delay;
     private void Start()
     {
+        Material MyMaterial = GetComponent<Renderer>().sharedMaterial;
+        GameObject[] Slimes = GameObject.FindGameObjectsWithTag("Slime");
+        float Distance = float.MaxValue;
+        foreach(GameObject k in Slimes)
+        {
+            if (k.GetComponent<Renderer>().sharedMaterial != MyMaterial)
+            {
+                if(Vector3.Distance(k.transform.position, transform.position) < Distance)
+                {
+                    Target = k.transform;
+                    Distance = Vector3.Distance(k.transform.position, transform.position);
+                }
+            }
+        }
         delay = Delay;
     }
     void Update()
@@ -18,8 +32,30 @@ public class AbominacjaRobiacaZaPathFind : MonoBehaviour
             if (delay <= 0)
             {
                 Vector3 Way = Target.position - transform.position;
-                if (Mathf.Abs(Way.x) > Mathf.Abs(Way.z)) { transform.position += Vector3.right * Mathf.Sign(Way.x); }
-                else { transform.position += Vector3.forward * Mathf.Sign(Way.z); }
+                if (Mathf.Abs(Way.x) > Mathf.Abs(Way.z)) 
+                {
+                    Ray ray = new Ray(transform.position, Vector3.right * Mathf.Sign(Way.x));
+                    if(!Physics.Raycast(ray, 1.0f))
+                    { transform.position += Vector3.right * Mathf.Sign(Way.x); }
+                    else
+                    {
+                        ray = new Ray(transform.position, Vector3.forward * Mathf.Sign(Way.z));
+                        if (!Physics.Raycast(ray, 1.0f))
+                        { transform.position += Vector3.forward * Mathf.Sign(Way.z); }
+                    }
+                }
+                else 
+                {
+                    Ray ray = new Ray(transform.position, Vector3.forward * Mathf.Sign(Way.z));
+                    if (!Physics.Raycast(ray, 1.0f))
+                    { transform.position += Vector3.forward * Mathf.Sign(Way.z); }
+                    else
+                    {
+                        ray = new Ray(transform.position, Vector3.right * Mathf.Sign(Way.x));
+                        if (!Physics.Raycast(ray, 1.0f))
+                        { transform.position += Vector3.right * Mathf.Sign(Way.x); }
+                    }
+                }
                 delay = Delay;
             }
             delay -= Time.deltaTime;
