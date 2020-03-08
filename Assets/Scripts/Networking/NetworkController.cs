9011c4bfb97cd46bc2d169741f3a79a38409fac1
nNown <wojciechmar2000@gmail.com>
2020-03-08 22:06:24 +0100

    Lobby ui

--- Assets/Scripts/Networking/NetworkController.cs
+++ Assets/Scripts/Networking/NetworkController.cs
@@ -1,28 +1,30 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworkController : MonoBehaviourPunCallbacks
-{
-    void Start()
-    {
+public class NetworkController : MonoBehaviourPunCallbacks {
+    void Start() {
         PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        RoomOptions RoomSet = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)16 };
+        PhotonNetwork.CreateRoom("Room", RoomSet);
+
+        PhotonNetwork.JoinRoom("Room");
     }
 
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Yaaay! Connected to: " + PhotonNetwork.CloudRegion);
-    }
-    /*
-        TypedLobby MyLobby = new TypedLobby("Lobby", LobbyType.Default);
-        PhotonNetwork.JoinLobby(MyLobby);
+    public override void OnConnectedToMaster() {
+        Debug.Log("Connected to: " + PhotonNetwork.CloudRegion);
     }
 
-    public override void OnJoinedLobby()
-    {
-        Debug.Log("Yaaaay!x2 Joined to Lobby " + PhotonNetwork.CurrentLobby.Name);
+    public override void OnCreateRoomFailed(short returnCode, string message) {
+        Debug.Log("Failed to create room, code: " + returnCode);
     }
-    */
+
+    public override void OnJoinRoomFailed(short returnCode, string message) {
+        Debug.Log("Failed to join room, code: " + returnCode);
+    }
 }
