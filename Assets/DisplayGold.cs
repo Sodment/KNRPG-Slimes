21db9141ce78faf6be2aeaf9d428ab75a5d3e613
Sodment <57872802+Sodment@users.noreply.github.com>
2020-04-11 22:23:00 +0200

    Map Update

---
+++ Assets/DisplayGold.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayGold : MonoBehaviour
+{
+    Text goldtext;
+    int player1gold;
+    int player2gold;
+    int currentplayer;
+    void Start()
+    {
+        goldtext = GetComponent<Text>();
+        player1gold = GameObject.Find("CameraHandler").GetComponent<PVPGameMenager>().goldplayer1;
+        player2gold = GameObject.Find("CameraHandler").GetComponent<PVPGameMenager>().goldplayer2;
+        currentplayer = (int)GameObject.Find("CameraHandler").GetComponent<PVPGameMenager>().CurrentStage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player1gold = GameObject.Find("CameraHandler").GetComponent<PVPGameMenager>().goldplayer1;
+        player2gold = GameObject.Find("CameraHandler").GetComponent<PVPGameMenager>().goldplayer2;
+        currentplayer = (int)GameObject.Find("CameraHandler").GetComponent<PVPGameMenager>().CurrentStage;
+        if (currentplayer == 0){ goldtext.enabled = true;  goldtext.text = "Gold: " + player1gold.ToString(); }
+        else if(currentplayer == 2){ goldtext.enabled = true; goldtext.text = "Gold: " + player2gold.ToString(); }
+        else { goldtext.enabled = false; }
+    }
+}
